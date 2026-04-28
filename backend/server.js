88fbe93eae8815966caedf2ff6aaeb7615197003
Joyce M. Villanueva <jmellor3813@dmmmsu.edu.ph>
2026-04-28 11:24:00 +0800

    Add root route

--- backend/server.js
+++ backend/server.js
@@ -38,3 +38,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 console.log(`Server is running on ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
