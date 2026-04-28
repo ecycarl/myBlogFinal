889ca96a53a2afe71e899022f9783a383fd1c27c
Joyce M. Villanueva <jmellor3813@dmmmsu.edu.ph>
2026-04-28 15:17:18 +0800

    update

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,18 @@ app.use(cors({
   credentials: true
 }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 // BODY PARSER
 app.use(express.json());
 
