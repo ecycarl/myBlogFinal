4504d2f33d94205ddae6c0afc463907e73cdacb3
Joyce M. Villanueva <jmellor3813@dmmmsu.edu.ph>
2026-04-28 14:33:56 +0800

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 // frontend/src/api/axios.js
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: "https://myblogfinal-78hq.onrender.com/api",
+  baseURL: "https://myblogfinal-12xh.onrender.com/api",
 });
 
 instance.interceptors.request.use((config) => {
